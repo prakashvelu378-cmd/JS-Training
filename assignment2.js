7765e1eee908bd13b7f65e4730f717abfdc73281
prakashvelu378-cmd <prakashvelu378@gmail.com>
2026-08-27 16:38:31 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,28 @@
+//task 1
+let studentName = "Krishna";
+let studentAge = 22;
+let mark = 85;
+console.log(studentName);
+console.log(studentAge);
+console.log(mark);
+ 
+ 
+//task 2
+let name = prompt("Enter your name:");
+alert(name);
+ 
+ 
+//task 3
+let age = prompt("Enter your age:");
+console.log(age);
+ 
+ 
+//task 4
+let  a = confirm("How are yoo?");
+console.log(a);
+ 
+ 
+//task 5
+let b = prompt("Enter your name again:");
+document.writeln(b);
+ 
